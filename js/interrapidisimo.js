d123d502d8290626bc01e2ff0d4175af783961cc
katiuskizzz <munozkatalinavalderrama@gmail.com>
2023-05-24 12:43:55 -0500

    formulario

---
+++ js/interrapidisimo.js
@@ -0,0 +1,15 @@
+let hambuerguerMenu = document.getElementById("hambuerguerMenu");
+let mainMenu = document.getElementById("mainMenu");
+
+hambuerguerMenu.addEventListener("click", function() {
+
+        if(mainMenu.classList.contains("main-menu")){
+        mainMenu.classList.add("main-menu-block");
+        mainMenu.classList.remove("main-menu");
+     }
+    else{
+        mainMenu.classList.remove("main-menu-block");
+        mainMenu.classList.add("main-menu");
+    
+    }
+});
