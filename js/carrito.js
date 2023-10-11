729da999d493d9b5223bd717338af06138a5c0c1
katiuskizzz <munozkatalinavalderrama@gmail.com>
2023-10-11 16:28:03 -0500

    Carrito de compras

--- js/carrito.js
+++ js/carrito.js
@@ -3,9 +3,18 @@
 let boxMinus = document.getElementById("boxMinus")
 let boxCantidad = document.getElementById("boxCantidad")
 let boxPlus = document.getElementById("boxPlus")
-    // console.log("vengo de la consola") ir a inspeccionar y hundir consola y el objeto que hara eso
+let numeroProductos = document.getElementById("numeroProductos")
+let valorProducto = 239997;
+let auxValorProducto = new Intl.NumberFormat("de-DE").format(valorProducto)
+document.getElementById("precioUnidad").innerHTML = auxValorProducto;
+
+
+
+   // console.log("vengo de la consola") ir a inspeccionar y hundir consola y el objeto que hara eso
     // alert("holis") esta añlerta aparece arriba de la pagina 
     // alert(document.getElementById("boxCantidad").innerHTML) PARA SABER SI ESTA LEYENDO LOS VALORES
+
+
 
 boxPlus.addEventListener("click", function(){
 
@@ -13,6 +22,13 @@ boxPlus.addEventListener("click", function(){
     if(auxCant < 10){
         auxCant++;
         boxCantidad.innerHTML = auxCant
+       determinarSingularPlural(auxCant)
+
+      let multi = valorProducto * auxCant,
+    auxSubTotal = new Intl.NumberFormat("de-DE").format(multi);
+
+    document.getElementById("subtotal").innerHTML = auxSubTotal;
+
     }
 
 
@@ -23,5 +39,23 @@ boxMinus.addEventListener("click",function(){
     if(auxCanti > 0){
         auxCanti--;
         boxCantidad.innerHTML = auxCanti
+        determinarSingularPlural(auxCanti)
+
+        
+      let resta =  valorProducto * auxCanti,
+      auxSubTotal = new Intl.NumberFormat("de-DE").format(resta);
+  
+      document.getElementById("subtotal").innerHTML = auxSubTotal;
+
     }
+     
 })
+function determinarSingularPlural(auxCant){
+    if(auxCant == 1){
+        numeroProductos.innerHTML = "(" + auxCant + " Producto)"
+    }
+    else if ((auxCant== 0 || auxCant >1 )){
+        numeroProductos.innerHTML = "(" + auxCant + " Productos)"
+    }   
+}
+
