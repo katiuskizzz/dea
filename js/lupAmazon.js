16423ceed79d7042ff063dfcf63cf27dd6923fc0
katiuskizzz <munozkatalinavalderrama@gmail.com>
2023-11-01 16:32:23 -0500

    cambio de imagen js

--- js/lupAmazon.js
+++ js/lupAmazon.js
@@ -1,3 +1,47 @@
-//alert ("sirve")
+let btnClose = document.getElementById("btnClose")
+let mainContent = document.getElementById("mainContent")
 
-let 
+
+
+// Event Listener for the close button
+btnClose.addEventListener('click', function(){
+    mainContent.style.display = "none";
+    });
+
+
+let boxImg1 = document.getElementById("boxImg1")
+let boxImg2 = document.getElementById("boxImg2")
+let boxImg3 = document.getElementById("boxImg3")
+let boxImg4 = document.getElementById("boxImg4")
+let boxImg5 = document.getElementById("boxImg5")
+let boxImg6 = document.getElementById("boxImg6")
+let boxImg7 = document.getElementById("boxImg7")
+let boxImg8 = document.getElementById("boxImg8")
+let mainImgModal = document.getElementById("mainImgModal")
+
+boxImg1.addEventListener("click", () =>{
+    mainImgModal.setAttribute("src", "img/Nueva carpeta/laptop1.jpg")
+})
+
+boxImg2.addEventListener("click", () =>{
+    mainImgModal.setAttribute("src", "img/Nueva carpeta/laptop2.jpg")
+})
+boxImg3.addEventListener("click", () =>{
+    mainImgModal.setAttribute("src", "img/Nueva carpeta/laptop3.jpg")
+})
+
+boxImg4.addEventListener("click", () =>{
+    mainImgModal.setAttribute("src", "img/Nueva carpeta/laptop4.jpg")
+})
+boxImg5.addEventListener("click", () =>{
+    mainImgModal.setAttribute("src", "img/Nueva carpeta/laptop5.jpg")
+})
+boxImg6.addEventListener("click", () =>{
+    mainImgModal.setAttribute("src", "img/Nueva carpeta/laptop6.jpg")
+})
+boxImg7.addEventListener("click", () =>{
+    mainImgModal.setAttribute("src", "img/Nueva carpeta/laptop7.jpg")
+})
+boxImg8.addEventListener("click", () =>{
+    mainImgModal.setAttribute("src", "img/Nueva carpeta/mousepadlaptop.jpg  ")
+})
